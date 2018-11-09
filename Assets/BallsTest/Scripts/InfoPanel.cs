ff5492a8400f03d1cae1d62aea816a673702d63e
Nashet <Nashet@users.noreply.github.com>
2018-11-09 23:33:49 +0300

    Added lefy panel with scores and time

---
+++ Assets/BallsTest/Scripts/InfoPanel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace BallsTest
+{
+    internal class InfoPanel : MonoBehaviour
+    {
+        [SerializeField] protected Text timeLeft, scores;
+
+        private void Update()
+        {
+            timeLeft.text = GameLogic.Instance.TimeLeft.ToString("0.00");
+            scores.text = GameLogic.Instance.PlayerScores.ToString();
+        }
+    }
+}
