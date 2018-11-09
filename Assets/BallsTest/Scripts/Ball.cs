218efed6b38a20dc600d02ebd29897340fb25f99
Nashet <Nashet@users.noreply.github.com>
2018-11-09 20:04:52 +0300

    Added balls generator

---
+++ Assets/BallsTest/Scripts/Ball.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace BallsTest
+{
+    /// <summary>
+    /// Represents ball's behaviour
+    /// </summary>
+    public class Ball : MonoBehaviour
+    {
+        [SerializeField] private float size, speed;
+        public void Setup(float size, Color color, Vector2 position)
+        {
+            
+            this.size = size;
+            var image = gameObject.AddComponent<Image>();
+            image.color = color;
+
+            var rectTransform = GetComponent<RectTransform>();
+            rectTransform.pivot = Vector2.zero;
+            rectTransform.anchorMin = Vector2.zero;
+            rectTransform.anchorMax = Vector2.zero;
+            rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, size);
+            rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, size);
+
+            rectTransform.anchoredPosition = position;
+        }
+
+        private void FixedUpdate()
+        {
+            //todo if outside window - destroy
+            //Destroy(this);           
+        }
+    }
+}
---
+++ Assets/BallsTest/Scripts/BallsGenerator.cs
@@ -0,0 +1,50 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace BallsTest
+{
+    /// <summary>
+    /// Generates balls with given properties
+    /// </summary>
+    [RequireComponent(typeof(RectTransform))]
+    internal class BallsGenerator : MonoBehaviour
+    {
+        [Tooltip("In seconds")]
+        [Range(0f, 10f)]
+        [SerializeField] protected float ballsAppearanceDelay;
+
+        [Range(5f, 50f)]
+        [SerializeField] protected float ballMinSize, ballMaxSize;
+
+        ///<summary>Area where balls can flow</summary>
+        protected RectTransform gameField;
+
+        // Use this for initialization
+        protected void Start()
+        {
+            gameField = GetComponent<RectTransform>();
+            StartCoroutine(GenerateBallsCoroutine());
+        }
+
+        protected IEnumerator GenerateBallsCoroutine()
+        {
+            while (true)
+            {
+                var @object = new GameObject("Ball");
+                @object.transform.parent = this.transform;
+                var ball = @object.AddComponent<Ball>();
+
+                var ballSize = Random.Range(ballMinSize, ballMaxSize);
+
+                // spawns balls below lower gameField border
+                // (0,0) is left lower corner of gameField
+                var ballPosition = new Vector2(Random.Range(0, gameField.rect.width - ballSize), -ballSize);
+
+                ball.Setup(ballSize, Random.ColorHSV(), ballPosition);
+
+                yield return new WaitForSeconds(ballsAppearanceDelay);
+            }
+        }
+    }
+}
